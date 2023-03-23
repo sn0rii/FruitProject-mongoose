45ba69af665f15b9e86d3c125ceda66311b13fe8
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-03-23 09:15:28 +0100

    update

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 // const mongoose = require("mongoose");
 // mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 
+// require("events").EventEmitter.prototype._maxListeners = 70;
+// require("events").defaultMaxListeners = 70;
+
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {
   useNewUrlParser: true,
 });
 
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified!"],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
   review: String,
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
-  review: " Pretty solid as a fruit.",
+  rating: 10,
+  review: " Peaches are so yummy!",
 });
 
-//fruit.save();
+// fruit.save();
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -36,30 +45,50 @@ const person = new Person({
 
 // person.save();
 
-const kiwi = new Fruit({
-  name: "kiwi",
-  score: 10,
-  review: "The best fruit!",
-});
-const banana = new Fruit({
-  name: "banana",
-  score: 9,
-  review: "NIce fruit!",
-});
-const orange = new Fruit({
-  name: "orange",
-  score: 10,
-  review: "Top fruit!",
-});
-
-Fruit.insertMany([kiwi, banana, orange]);
-
-//   function (err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Succes!");
-//   }
+// const kiwi = new Fruit({
+//   name: "kiwi",
+//   score: 10,
+//   review: "The best fruit!",
 // });
+// const banana = new Fruit({
+//   name: "banana",
+//   score: 9,
+//   review: "Nice fruit!",
+// });
+// const orange = new Fruit({
+//   name: "orange",
+//   score: 10,
+//   review: "Top fruit!",
+// });
+
+// // Fruit.insertMany([kiwi, banana, orange]);
+
+// //   function (err) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log("Succes!");
+// //   }
+// // });
+
+Fruit.find()
+  .then(function (fruits) {
+    // console.log(fruits);
+    fruits.forEach(function (fruit) {
+      console.log(fruit.name);
+      mongoose.connection.close();
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+// Fruit.find(function (fruits) {
+//   console.log(fruits);
+// });
+
+Fruit.updateOne({ _id: "641bf62bb875d5d6a2c4e8a7" }, { name: "Peach" });
+
+// Fruit.deleteOne({ _id: "641bef7aa058db48f6816be4" });
 
 console.log("Serwer smiga");
